af54cf66749c3e76be6ca1fb553b3b284be3da54
AlexSchifano <alexandre.schifano.w@gmail.com>
2025-12-10 17:53:16 +0100

    Delete S3_align_mafft.sh

--- S3_align_mafft.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /Users/alesc399/Documents/C.elegans_sync/Final_files/01-DATA
-## alignement
-
-mafft --auto --preservecase 3-MS_FASTA/fasta_merged_wild_2O.fasta > 4-ALIGN/MSA_fasta_merged_wild_2O.fasta
-mafft --auto --preservecase 3-MS_FASTA/fasta_merged_wild_only.fasta > 4-ALIGN/MSA_fasta_merged_wild_only.fasta
